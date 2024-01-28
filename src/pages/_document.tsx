1d24c2cdf03d16b13b8a3c39e75ddb2ba20bbdd2
Thorsten Lorenz <thlorenz@gmx.de>
2024-01-27 21:41:07 -0700

    prep: for gh-pages deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
           name="description"
           content="Luzid: Solana Development at Light Speed Example"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/ex-tictactoe.web/favicon.ico" />
       </Head>
       <body>
         <Main />
