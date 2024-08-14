780a112909463b4129383d98ad6b3930a600a008
Thorsten Lorenz <thlorenz@gmx.de>
2024-08-14 16:38:28 +0400

    style: smaller buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ export default function Button(props: {
     <div className="rounded-md shadow-sm">
       <button
         type="button"
-        className="px-4 py-2 my-4 w-64 text-sm font-medium text-gray-900 border border-gray-200 rounded-e-lg rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        className="px-2 py-2 my-4 w-28 text-sm font-medium text-gray-900 border border-gray-200 rounded-e-lg rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
         onClick={props.onClick}
       >
         {props.children}
